bf0f67252014567af69360fd380fdb0665eab0ad
iamjink <jinijinjin@gmail.com>
2020-02-02 20:22:23 -0800

    getting javascript buttons to show up and gifs to show on click

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-$(document).ready(function() {
-var 
+$(document).ready(function () {
+    //buttons array to display on page load
+    var scienceArr = ["asteroids", "engineering", "nuclear", "astronomy", "nasa", "magnet", "computer", "mathematics", "space", "robot", "diy"];
 
-$("button").on("click", function() {
+    //buttons appear on page load
+    function showButtons() {
+        for (var i = 0; i < scienceArr.length; i++) {
+            var button = $("<button>");
+            button.attr("data-science", scienceArr[i]);
+            button.text(scienceArr[i]);
+            $("#buttonsList").append(button);
+        };
+    };
 
+    //showGifts on button click
+    function showGifs() {
+        $("button").on("click", function () {
+            var science = $(this).attr("data-science");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                science + "&api_key=chDuK3TIQSCJCXwnaOChRtivwgMr2alW&limit=10";
+            console.log(queryURL);
+
+            $("#gifsList").empty();
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                console.log(response);
+
+
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var scienceImage = $("<img>");
+                    scienceImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(scienceImage);
+
+                    $("#gifsList").prepend(gifDiv);
+                }
+
+
+            });
+        });
+    }
+
+    showButtons();
+    showGifs();
 
 
 });
-
-
-
-
-});
-
